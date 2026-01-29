c649efa66d0fc754616a0178134a6d60cdb69e03
Jorge Núñez <94089914+jphants@users.noreply.github.com>
2026-01-28 22:50:24 -0500

    Stuff

--- Level/level_1.gd
+++ Level/level_1.gd
@@ -3,8 +3,7 @@ extends Node3D
 @export var building_scene: PackedScene
 
 func _ready():
-	spawn(Vector3(10, 0, 5))
-	spawn(Vector3(20, 0, 5))
+	pass
 
 func spawn(pos):
 	var b = building_scene.instantiate()
